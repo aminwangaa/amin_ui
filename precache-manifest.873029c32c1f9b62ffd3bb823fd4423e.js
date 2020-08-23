e48dd5e897f24a8c0762ad0ade154db9f464b511
aminwangaa <42959482+aminwangaa@users.noreply.github.com>
2020-08-23 03:48:15 +0000

    Deploying to gh-pages from  @ d969ec71217f20b495c5b6f6d2c412e302ce8abb 🚀

---
+++ precache-manifest.873029c32c1f9b62ffd3bb823fd4423e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a614130bdc5ffdb0190a07e7ceb090",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.058d33c2de5dcc4f1c21.bundle.js"
+  },
+  {
+    "url": "runtime~main.058d33c2de5dcc4f1c21.bundle.js"
+  },
+  {
+    "revision": "224464a30395060c251f8b864ab2b3e2",
+    "url": "static/media/cat.224464a3.jpg"
+  },
+  {
+    "revision": "c41e07697a9383d4f4e3d157832a8eea",
+    "url": "static/media/cat.c41e0769.jpeg"
+  },
+  {
+    "url": "vendors~main.058d33c2de5dcc4f1c21.bundle.js"
+  },
+  {
+    "url": "vendors~main.058d33c2de5dcc4f1c21.bundle.js.LICENSE.txt"
+  }
+]);
